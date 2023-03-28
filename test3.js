d0a7db4f1d9a110c276e4e8dd6b649b927f52b28
Josh Johanning <joshjohanning@github.com>
2023-03-28 09:34:16 -0500

    i'm adding a new line

--- test3.js
+++ test3.js
@@ -1,5 +1,6 @@
 function endsWith(x,y) {
 
+    let newVar = "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";
     let index = x.lastIndexOf(y);
     return x.lastIndexOf(y) === x.length - y.length;
   
