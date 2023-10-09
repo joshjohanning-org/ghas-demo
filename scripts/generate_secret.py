0a00205677b7e224164142c8820dba9186c92954
joshgoldfishturtle <kob24ever@outlook.com>
2023-10-09 15:11:04 -0500

    Update generate_secret.py

--- scripts/generate_secret.py
+++ scripts/generate_secret.py
@@ -4,3 +4,5 @@ import rstr
 print(f"AWS_KEYID: {rstr.xeger(r'AKIA[A-Z2-7]{16}')}")
 print(f"AWS_SECRET: {rstr.xeger(r'[0-9A-Za-z/+=]{40}')}")
 print(f"DATADOG_TOKEN: {rstr.xeger(r'[a-f0-9]{32}|[a-f0-9]{40}')}")
+
+print(f"GITHUB PAT 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")
